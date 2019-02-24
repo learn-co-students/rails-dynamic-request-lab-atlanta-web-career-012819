b167cd3b1b076cc5abedc32c5233d8b19ae9218d
Megan Fago <meganlaynefago@gmail.com>
2019-02-24 11:14:36 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,19 @@
 class StudentsController < ApplicationController
+  ### not exactly sure what this does??
+    before_action :set_student, only: :show
   def index
     @students = Student.all
+  end
+
+  def show
+
+  end
+
+## this is what the solution had but I'm not sure what the
+## private method does??
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
